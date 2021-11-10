8a693d6543fcad8bd85a9c0f33075d96800fe48e
Roger Medrano <email@example.com>
2021-11-09 17:54:06 -0700

    Classes Ex1

---
+++ Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+
+    }
+    
+      
+}
---
+++ Car/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+            //CLASSES EXERCISE:  CARS
+
+            //In the Car class: Create a Make property of type string that is public
+                                //Create a Model property of type string that is public
+                                //Create a Year property of type int that is public
+
+        {
+            var firstCar = new Car();
+
+            //another way to script the above line:  Car firstCar = new Car();
+            firstCar.Make = "Ford";
+            firstCar.Model = "Explorer";
+            firstCar.Year = 2004;
+            Console.WriteLine($"Make: {firstCar.Make}");
+            Console.WriteLine($"Model: {firstCar.Model}");
+            Console.WriteLine($"Year: {firstCar.Year}");
+        }
+    }
+}
